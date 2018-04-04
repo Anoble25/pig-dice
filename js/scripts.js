0240585171adc3d1818506239d0d327a023611b9
Guest User <Guest@epicodus-15.local>
2018-04-04 09:11:18 -0700

    add roll functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
+function roll(){
+  return Math.floor(Math.random()*6) + 1;
+}
+
 $(document).ready(function() {
-
-
-  $("#").submit(function(event){
-
+  // $("#rollButton").submit(function(event) {
+  //   event.preventDefault();
+  //   console.log(roll());
+  //   alert("submit works");
+  // });
+  
+  $("#rollButton").click(function(){
+    console.log("roll works");
+    event.preventDefault();
+    var temp=roll();
+    $("#rollList").append("<li>" + temp+"</li>");
+    console.log(temp);
   });
 });
+
+// This works
+// $(document).ready(function() {
+//   $("form#playForm").submit(function(event) {
+//     event.preventDefault();
+//     var temp=roll();
+//     console.log(temp);
+//   });
+// });

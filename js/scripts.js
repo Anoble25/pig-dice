64694ac48186a107e50bd3f99ed5667fc5005ce8
Guest User <Guest@epicodus-15.local>
2018-04-04 09:17:45 -0700

    add turn total

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+var turnScores=[];
+
+
 function roll(){
   return Math.floor(Math.random()*6) + 1;
+}
+
+function updateTurnTotal(){
+  var tempTotal=0;
+  turnScores.forEach(function(num){
+    tempTotal+=num;
+  });
+  return tempTotal;
 }
 
 $(document).ready(function() {
@@ -8,14 +19,18 @@ $(document).ready(function() {
   //   console.log(roll());
   //   alert("submit works");
   // });
-  
+
   $("#rollButton").click(function(){
     console.log("roll works");
     event.preventDefault();
     var temp=roll();
     $("#rollList").append("<li>" + temp+"</li>");
     console.log(temp);
+    turnScores.push(temp);
+    $("#rollList").text(updateTurnTotal());
   });
+
+
 });
 
 // This works
